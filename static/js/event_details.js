9bb691bcc3187061cd6ad0a8c73be2f878fb9173
Anthony <ajackerm@mtu.edu>
2025-10-04 20:50:44 -0400

    update

--- static/js/event_details.js
+++ static/js/event_details.js
@@ -39,17 +39,17 @@
   }
 
   // Make sticky bar appear after hero title scrolls out
-  const sticky = document.querySelector("[data-stickys]");
-  const title = document.querySelector(".event-title");
-  if (sticky && title) {
-    const obs = new IntersectionObserver(
-      entries => {
-        entries.forEach(entry => {
-          sticky.style.opacity = entry.isIntersecting ? "0.0" : "1.0";
-          sticky.style.pointerEvents = entry.isIntersecting ? "none" : "auto";
-        });
-      }, { threshold: 0.05 }
-    );
-    obs.observe(title);
-  }
+  // const sticky = document.querySelector("[data-stickys]");
+  // const title = document.querySelector(".event-title");
+  // if (sticky && title) {
+  //   const obs = new IntersectionObserver(
+  //     entries => {
+  //       entries.forEach(entry => {
+  //         sticky.style.opacity = entry.isIntersecting ? "0.0" : "1.0";
+  //         sticky.style.pointerEvents = entry.isIntersecting ? "none" : "auto";
+  //       });
+  //     }, { threshold: 0.05 }
+  //   );
+  //   obs.observe(title);
+  // }
 })();
